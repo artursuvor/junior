c177d2e38075d064463db979fbe1d18adb167cb6
Fallougin <artursuvor@mail.ru>
2023-11-21 14:45:08 +0100

    3 task complete

---
+++ tasks/task_3/Person.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor (jmeno, vek){
+    this.jmeno = jmeno
+    this.vek = vek
+  }
+  toString(){
+    return `Jmenuji se ${this.jmeno} a je mi ${this.vek} let`
+  }
+}
+
+module.exports = Person
