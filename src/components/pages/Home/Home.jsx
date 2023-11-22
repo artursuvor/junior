11a28a1010524b44216eacf11b1d40b685103f97
Fallougin <artursuvor@mail.ru>
2023-11-22 12:52:50 +0100

    Task 4 completed

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,26 +1,46 @@
-import { useState, useEffect } from 'react';
-import { Button, Icon } from '../../shared';
+import { useState } from 'react';
+import { Button, Icon, UserList } from '../../shared';
 import style from './Home.module.css';
 
-function Home() {
-  const [userData, setData] = useState([]);
+export default function Home() {
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   function fetchData() {
+    setLoading(true);
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => {
+        setUserData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
   }
 
-  console.log(userData);
+  const userElements = userData.map((user) => {
+    return (
+      <UserList
+        key={user.id}
+        name={user.name}
+        email={user.email}
+        address={user.address}
+        phone={user.phone}
+      />
+    );
+  });
+
   return (
     <main className={style.home}>
-      <h1>User list</h1>
+      <h1 className={style.title}>User list</h1>
       <div>
         <Button variant={'secondary'} onClick={fetchData}>
           <Icon iconName="download" />
         </Button>
       </div>
+      {loading ? <Icon iconName="search" className={style.icon} /> : userElements}
     </main>
   );
 }
-export default Home;
--- src/components/shared/UserList/UserList.jsx
+++ src/components/shared/UserList/UserList.jsx
@@ -1,10 +1,21 @@
-import { React, useState, useEffect } from 'react';
+import styles from './UserList.module.css';
 
-function UserList(props){
+function UserList(props) {
+  const { name, email, phone, address } = props;
+  const mapUrl = `https://mapy.cz/turisticka?source=coor&id=${address.geo.lng}%2C${address.geo.lat}&x=${address.geo.lng}&y=${address.geo.lat}&z=16`;
 
   return (
-    <div></div>
-  )
-
-};
+    <div className={styles.container}>
+      <h2 className={styles.title}>User Details</h2>
+      <div className={styles.details}>
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <p>Phone: {phone}</p>
+        <p>
+          Location: <a className={styles.link} href={mapUrl} target="_blank" rel="noopener noreferrer">Show on map</a>
+        </p>
+      </div>
+    </div>
+  );
+}
 export default UserList;
